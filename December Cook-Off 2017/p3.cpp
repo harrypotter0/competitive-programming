b7faa3561f0842acc6e6b282f12e83af83fd9e4c
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-03-23 00:05:31 +0530

    NITD Reverse Coding v2.0

--- December Cook-Off 2017/p3.cpp
+++ December Cook-Off 2017/p3.cpp
@@ -1,35 +1,68 @@
-#include "bits/stdc++.h"
-using namespace std;
+/*
+ *
+ ********************************************************************************************
+ * AUTHOR : Akash Kandpal                                                                    *
+ * Language: C++14                                                                          *
+ * Motto : The master has failed more times than the beginner has even tried.               *                                                                *
+ * IDE used: Atom                                                                           *
+ * My Domain : http://harrypotter.tech/                                                     *
+ ********************************************************************************************
+ *
+ */
+    #include <bits/stdc++.h>
 
-int t;
-deque<pair<long long int, int> > q;
+    using namespace std;
 
-int main() {
-  cin >> t;
-  while (t--) {
-    q.clear();
-    long long int n,d;
-    scanf("%lld%lld", &n, &d);
-    long long int sum = 0;
-    q.push_back(make_pair(sum, -1));
-    int ans = INT_MAX;
-    for (int i = 0; i < n; i++) {
-      long long int a;
-      scanf("%lld", &a);
-      sum += a;
-      while (q.size()&&sum - q.front().first >= d) {
-        ans = min(ans, i - q.front().second);
-        q.pop_front();
-      }
-      while (q.size() && q.back().first > sum) {
-        q.pop_back();
-      }
-      q.push_back(make_pair(sum, i));
+    const int inf = 1e9;
+
+    void print1(auto output){
+        for(int i = 0; i < output.size(); i++)
+{
+     cout <<"["<< output[i].first << ", " << output[i].second <<"]";
+}
+cout<<endl;
     }
-    if (ans == INT_MAX) {
-      ans = -1;
+        void print2(auto output){
+            cout<<"[";
+        for(int i = 0; i < output.size(); i++)
+{
+     cout << output[i];
+}
+cout<<"]";
+cout<<endl;
     }
-    printf("%d\n", ans);
-  }
-  return 0;
-} 
+
+    int main(int argc, char *argv[]) {
+      int T;
+      cin >> T;
+      while (T--) {
+        int n, d;
+        cin >> n >> d;
+        vector<int> a(n + 1, 0);
+        vector<pair<int, int>> st(1, {0, 0});
+        int ans = inf;
+        for (int i = 1; i <= n; i++) {
+          cin >> a[i];
+          a[i] += a[i - 1];
+          auto it = lower_bound(st.begin(), st.end(), pair<int, int>{a[i] - d, inf});
+        //   cout <<(it-st.begin()) ;
+        //   cout<<"values===>"<<st[(it-st.begin())].first << " "<< st[(it-st.begin())].second<<"<==values ";
+          if (it != st.begin()) {
+            --it;
+            // cout<<"nhi hai ";
+            ans = min(ans, i - it->second);
+            // cout<<"i "<<i<<" "<<it->second<<"second";
+            // cout<<"ans"<<ans<<"ans";
+          }
+          while (!st.empty() && st.back().first >= a[i]) {
+            //   cout<<" first"<<st.back().first<<" "<<a[i]<<"a[i]"<<" "<<i<<" i";
+              st.pop_back();
+          }
+        //   print1(st);
+          st.push_back({a[i], i});
+        //   print1(st);
+        }
+        cout << (ans == inf ? -1 : ans) << endl;
+      }
+      return 0;
+    }
